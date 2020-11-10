d515b8b6b035acf33487c5ec940b303c329d4ef7
YukiOchiai <xdi.grand.you.winner@gmail.com>
2020-11-10 18:40:09 +0900

    3層構造に変更の準備

---
+++ src/controller.ts
@@ -0,0 +1,8 @@
+import e from "express"
+
+export class Controller {
+
+    async create(req: e.Request){
+
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions'
 import * as express from 'express'
 import {database} from './ItemModel'
+import {log} from "firebase-functions/lib/logger";
 
 const app: express.Express = express()
 app.use(express.json())
@@ -8,37 +9,56 @@ const router: express.Router = express.Router()
 
 // 最初の入荷
 router.post('/create', (req:express.Request, res:express.Response) => {
-    database.add(req).then(_ =>
-        res.send({type:"SUCCESS"})
-    ).catch(_ =>
-        res.send({type:"ERROR"})
+    database.addInventory(req).then(data => {
+            res.status(200)
+            res.send({data:data})
+        }
+    ).catch(e => {
+            log(e.toString())
+            res.status(400)
+        }
     )
+})
+
+router.post('/createOrder',(req, res) => {
+    database.addOrder(req).then(data =>{
+        res.status(200)
+        res.send({data:data})
+    }).catch(e => {
+        log(e.toString())
+        res.status(400)
+    })
 })
 
 // 全ての取得
 router.post('/getAll', (req, res) => {
-    database.getAll(req).then(data => {
+    database.getAll().then(data => {
+        res.status(200)
         res.send(JSON.stringify(data))
-    }).catch(_ => {
-        res.send("ERROR")
+    }).catch(e => {
+        log(e.toString())
+        res.send(400)
     })
 })
 
 // 入荷　
 router.post('/increase', (req, res) => {
-    database.increase(req).then(num => {
+    database.increaseInventory(req).then(num => {
+        res.status(200)
         res.send({num:num})
-    }).catch(_ => {
-        res.send("ERROR")
+    }).catch(e => {
+        log(e.toString())
+        res.send(400)
     })
 })
 
 // 数を減らす
 router.post('/decrease', (req:express.Request, res:express.Response) => {
-    database.decrease(req).then(num => {
+    database.decreaseInventory(req).then(num => {
         res.send({num:num})
-    }).catch(_ => {
-        res.send("ERROR")
+    }).catch(e => {
+        log(e.toString())
+        res.send(400)
     })
 })
 
